2e85254c319bd10dd565399cbe6f75f8d287d642
Dave Steadman <davesteadman@me.com>
2024-06-27 23:47:43 +0100

    Fixed Camera Mover class

--- Code/Godot/FssCameraMover.cs
+++ Code/Godot/FssCameraMover.cs
@@ -3,11 +3,8 @@ using System;
 
 public partial class FssCameraMover : Node3D
 {
-    [Export]
-    public float MoveSpeed   = 0.6f; 
+    public float MoveSpeed   = 1.0f; // Tunable speed parameters
     public float RotateSpeed = 1.0f;
-
-    [Export]
     Vector3 direction = new Vector3();
     Vector3 rotation  = new Vector3();
 
@@ -18,60 +15,46 @@ public partial class FssCameraMover : Node3D
 
     public override void _Process(double delta)
     {
-        UpdateDirection();
+        UpdateInputs();
 
         if (direction.Length() > 0)
         {
             // Normalize the direction vector to prevent faster diagonal movement
             direction = direction.Normalized();
 
+            // Transform the direction from local space to global space
+            Vector3 forward = -Transform.Basis.Z;
+            Vector3 right   =  Transform.Basis.X;
+            Vector3 up      =  Transform.Basis.Y;
+            Vector3 globalDirection = (forward * direction.Z) + (right * direction.X) + (up * direction.Y);
+
             // Move the object
-            Position += direction * MoveSpeed * (float)(delta);
-
-
-            // Apply the movement to the basis of the object - moving relative to the object's current rotation
-            // Basis = GlobalTransform.Basis;
-            // Basis = Basis.Rotated(Vector3.Up, Mathf.DegToRad(rotation.Y));
-            // Basis = Basis.Rotated(Vector3.Right, Mathf.DegToRad(rotation.X));
-            // Basis = Basis.Rotated(Vector3.Forward, Mathf.DegToRad(rotation.Z));
-            // Position += Basis.Xform(direction) * MoveSpeed * (float)delta;
-
-
-            // Transform3D t = GlobalTransform;
-            // t.origin += t.basis.Xform(direction) * MoveSpeed * (float)delta;
-
-            // Apply the movement to the object's global transform - moving relative to the world's axes
-
-
-
-            //Vector3 movement = (GlobalTransform.Basis.Xform(direction)) * MoveSpeed * (float)delta;
-            //Position += movement;
-
+            Position += globalDirection * MoveSpeed * (float)(delta);
         }
-        if (rotation.Length() > 0) 
+        if (rotation.Length() > 0)
         {
             Rotation += rotation * RotateSpeed * (float)(delta);
         }
     }
 
-    private void UpdateDirection()
+    private void UpdateInputs()
     {
         direction = Vector3.Zero;
         rotation  = Vector3.Zero;
 
         if (Input.IsActionPressed("ui_shift"))
         {
-            if (Input.IsActionPressed("ui_up"))    direction.Z -= 1;
-            if (Input.IsActionPressed("ui_down") ) direction.Z += 1;
+            if (Input.IsActionPressed("ui_up"))    direction.Z += 1;
+            if (Input.IsActionPressed("ui_down") ) direction.Z -= 1;
         }
-        if (Input.IsActionPressed("ui_alt"))
+        else if (Input.IsActionPressed("ui_alt"))
         {
             if (Input.IsActionPressed("ui_up"))    rotation.X += 2f;
             if (Input.IsActionPressed("ui_down"))  rotation.X -= 2f;
             if (Input.IsActionPressed("ui_left"))  rotation.Y += 2f;
             if (Input.IsActionPressed("ui_right")) rotation.Y -= 2f;
         }
-        else 
+        else
         {
             if (Input.IsActionPressed("ui_left"))  direction.X -= 1;
             if (Input.IsActionPressed("ui_right")) direction.X += 1;
