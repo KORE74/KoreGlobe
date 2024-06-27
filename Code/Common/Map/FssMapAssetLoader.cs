edc3ddaa2abb115be0f7f9e65ba5f1b2f500d5b6
Dave Steadman <davesteadman@me.com>
2024-06-27 14:33:57 +0100

    Create FssMapAssetLoader.cs

---
+++ Code/Common/Map/FssMapAssetLoader.cs
@@ -0,0 +1,19 @@
+using System;
+
+using Godot;
+
+public static class FssMapAssetLoader
+{
+    public static string[] ConstDataTypes  = { "Ele", "SatImg" };
+    public static string[] ConstPathPerLvl = { "L0_30x30Degs/", "L1_5x5Degs/", "L2_1x1Degs/", "L3_0p2x0p2Degs/", "L4_0p04x0p04Degs/" };
+    
+    public void LoadElevationData(string rootDir, FssTileCode tileCode)
+    {
+    }
+
+    public Texture2D LoadMapTileTexture(string rootDir, FssTileCode tileCode)
+    {
+    }
+}
+
+
