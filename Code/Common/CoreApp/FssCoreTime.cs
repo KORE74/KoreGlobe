aaf58046a2df0f3a8d9b593a35d9c041e0025ee3
Dave Steadman <davesteadman@me.com>
2024-07-08 16:31:25 +0100

    Update FssCoreTime.cs

--- Code/Common/CoreApp/FssCoreTime.cs
+++ Code/Common/CoreApp/FssCoreTime.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 
 // FssCoreTime - A standard C# class to provide a runtime timer for use throughout the class framework.
@@ -13,13 +14,15 @@ public static class FssCoreTime
     }
 
     // Property to get the elapsed time in seconds since the application started
-    public static float RuntimeSecs
-    {
-        get
-        {
-            // Return the elapsed time in seconds
-            return (float)stopwatch.Elapsed.TotalSeconds;
-        }
-    }
-}
+    public static float RuntimeSecs           => (float)stopwatch.Elapsed.TotalSeconds;
 
+    // UTC Time and Data
+    public static string TimeOfDayHHMMSSUTC   => DateTime.UtcNow.ToString("HHmmss");
+    public static string DateYYYYMMDDUTC      => DateTime.UtcNow.ToString("yyyyMMdd");
+    public static string TimestampUTC         => $"{DateYYYYMMDDUTC}-{TimeOfDayHHMMSSUTC}";
+
+    // Local Time and Date
+    public static string TimeOfDayHHMMSSLocal => DateTime.Now.ToString("HHmmss");
+    public static string DateYYYYMMDDLocal    => DateTime.Now.ToString("yyyyMMdd");
+    public static string TimestampLocal       => $"{DateYYYYMMDDLocal}-{TimeOfDayHHMMSSLocal}";
+}
